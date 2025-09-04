19eb67d0ab6d09e13470ee37293ec1da23217b05
heraMo7amed4522 <eng.khaled.hera4522@gmail.com>
2025-09-04 16:59:07 +0400

    upload

---
+++ Luxor-App/Core/DI/DIContainer.swift
@@ -0,0 +1,7 @@
+//
+//  DIContainer..swift
+//  Luxor-App
+//
+//  Created by Standard User on 04/09/2025.
+//
+
---
+++ Luxor-App/Models/Photo.swift
@@ -0,0 +1,10 @@
+//
+//  Photos.swift
+//  Luxor-App
+//
+//  Created by Standard User on 04/09/2025.
+//
+
+import Foundation
+
+struct Photo: Codable, Identifiable{}
